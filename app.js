08b415c6b71ee688510ecaa8b947ae8436488e29
harsh <harshjain172001@gmail.com>
2022-06-16 10:16:46 +0530

    -ui changes

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
-const sections= document.querySelectorAll('.section');
-const secBtns=document.querySelectorAll('.controls');
-const secBtn=document.querySelectorAll('.control');
-const allSections=document.querySelector(".main-content");
+const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
 const btn = document.querySelector("#btn");
 const btnText = document.querySelector("#btnText");
 
 console.log("hi");
 
-
-function PageTransition()
-{
-    //Button click active class
-    for(let i=0;i<secBtn.length;i++)
-    {
-        secBtn[i].addEventListener("click",function()
-        {
-            let currentBtn=document.querySelectorAll(".active-btn");
-            currentBtn[0].classList=currentBtn[0].className.replace("active-btn",'');
-            this.className+=" active-btn";
-        });
-    }
-
-    //sections active class
-    allSections.addEventListener("click",(e)=>
-    {
-       const id=e.target.dataset.id;
-       if(id)
-       {
-           //remove selected from the other
-           secBtns.forEach((btn)=>
-           {
-                btn.classList.remove('active');
-           });
-           e.target.classList.add('active');
-
-           //hide other sections
-           sections.forEach((section)=>
-           {
-               section.classList.remove("current")
-           });
-
-           window.scrollTo(0,0);
-           const element=document.getElementById(id);
-           element.classList.add("current");
-       }
+function PageTransition() {
+  //Button click active class
+  for (let i = 0; i < secBtn.length; i++) {
+    secBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].classList = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
     });
+  }
 
+  //sections active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from the other
+      secBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("current");
+      });
+
+      window.scrollTo(0, 0);
+      const element = document.getElementById(id);
+      element.classList.add("current");
+    }
+  });
 }
 
 //FORM submission
@@ -57,16 +51,13 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // Success and Error functions for after the form is submitted
 
-  function success() 
-  {
+  function success() {
     btnText.innerHTML = "Success";
     btn.classList.add("active");
     form.reset();
-    
   }
 
-  function error() 
-  {
+  function error() {
     btnText.innerHTML = "Fill Captcha";
   }
 
@@ -96,23 +87,27 @@ function ajax(method, url, data, success, error) {
   xhr.send(data);
 }
 
-
-
 //theme toggle
-const themeBtn= document.querySelector('.toggle');
-themeBtn.addEventListener('click',()=>
-{
-    let elem=document.body;
-    elem.classList.toggle('light-mode');
-})
+const themeBtn = document.querySelector(".toggle");
+themeBtn.addEventListener("click", () => {
+  let elem = document.body;
+  elem.classList.toggle("light-mode");
+});
 
 //portfolio button
-const portfolioBtn=document.querySelectorAll(".language");
-portfolioBtn.forEach((ele)=>
-{
-  ele.addEventListener("click",()=>
-  {
+const portfolioBtn = document.querySelectorAll(".language");
+const allBtn = document.getElementById("all");
+allBtn.addEventListener("click", () => {
+  portfolioBtn.forEach((ele) => {
+    if (ele.textContent != "All") ele.classList.remove("selected");
+  });
+});
+portfolioBtn.forEach((ele) => {
+  ele.addEventListener("click", () => {
     ele.classList.toggle("selected");
+    if (ele.textContent != "All") {
+      allBtn.classList.remove("selected");
+    }
   });
 });
 
